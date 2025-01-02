3e236b1699a27dfd145b08a4939b9c63eebc9562
Thomas Maier <contact@thomas-maier.net>
2025-01-02 20:38:21 +0100

    refactored config module

--- unicorn/uclib/config.py
+++ unicorn/uclib/config.py
@@ -34,7 +34,7 @@ class Config:
             self.unicorn_type = Config.UnicornType.COSMIC
 
     def __str__(self):
-        return 'mqtt_host: %s, mqtt_port: %d, mqtt_client_id: %s, mqtt_topic_base: %s, mqtt_all_topic_base: %s, unicorn_type: %d, wifi_psk: ***, wifi_ssid: %s, wifi_country: %s' % (
+        return 'mqtt_host: %s, mqtt_port: %d, mqtt_client_id: %s, mqtt_topic_base: %s, mqtt_all_topic_base: %s, unicorn_type: %d, wifi_ssid: %s, wifi_country: %s' % (
             self.mqtt_host, self.mqtt_port, self.mqtt_client_id, self.mqtt_topic_base, self.mqtt_all_topic_base,
             self.unicorn_type,
             self.wifi_ssid, self.wifi_country)
