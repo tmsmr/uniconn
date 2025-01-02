1da91ac122f6a7930b04e537a024fe4ff05eedbf
Thomas Maier <contact@thomas-maier.net>
2025-01-02 17:36:07 +0100

    adjust testclient to new config structure/keys

--- testclients/publisher.py
+++ testclients/publisher.py
@@ -1,9 +1,10 @@
 import ssl
 import json
+from time import sleep, localtime
 
 import paho.mqtt.client as mqtt
 
-deployment = "../deployment/clients/tmp/desktop"
+deployment = "../infra/configs/tmp/desktop"
 
 with open(deployment + "/config.json") as f:
     config = json.load(f)
@@ -11,7 +12,7 @@ with open(deployment + "/config.json") as f:
 mqttc = mqtt.Client(
     callback_api_version=mqtt.CallbackAPIVersion.VERSION2,
     protocol=mqtt.MQTTv5,
-    client_id=config['client_id']
+    client_id=config['mqtt_client_id']
 )
 
 mqttc.tls_set(
@@ -25,8 +26,10 @@ mqttc.tls_set(
 def on_connect(client, userdata, flags, reason_code, properties):
     print("on_connect: " + str(reason_code))
 
+
 def on_log(client, userdata, level, buf):
     print("log: " + buf)
+
 
 mqttc.on_connect = on_connect
 mqttc.on_log = on_log
@@ -36,7 +39,10 @@ mqttc.connect(host=config['mqtt_host'], port=config['mqtt_port'])
 mqttc.loop_start()
 
 while True:
-    val = input("input: ")
-    for uc in config['uniconns']:
-        print("publishing " + uc['topic'] + ": " + val)
-        print(mqttc.publish(uc['topic'], val))
+    lt = localtime()
+    val = ':'.join([str(lt.tm_hour), str(lt.tm_min), str(lt.tm_sec)])
+    for uc in config['unicorns']:
+        topic = uc['topic_base'] + '/text'
+        print("publishing " + topic + ": " + val)
+        print(mqttc.publish(topic, val))
+    sleep(5)
